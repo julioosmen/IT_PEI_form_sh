9d59538e3257c6dbbd426301a0c543199f70dad4
Julio Osores <julio.osores61@gmail.com>
2026-01-20 14:15:15 -0500

    Update historial_sharepoint.py

--- adapters/historial_sharepoint.py
+++ adapters/historial_sharepoint.py
@@ -1,32 +1,32 @@
 import re
 import pandas as pd
 
-    APP_TO_EXCEL_HEADERS = {
-        "año": "Año",
-        "ng1": "N G 1",
-        "ng2": "N G 2",
-        "fecha_recepcion": "Fecha de recepción",
-        "periodo": "Periodo PEI ",
-        "vigencia": "Vigencia",
-        "tipo_pei": "Tipo de PEI",
-        "estado": "Estado ",
-        "responsable_institucional": "Responsable Institucional ",
-        "cantidad_revisiones": "Cantidad de revisiones",
-        "fecha_derivacion": "Fecha de derivación ",
-        "etapa_revision": "Etapas de revisión",
-        "comentario": "Comentario adicional/ Emisor de I.T",
-        "articulacion": "Articulación ",
-        "expediente": "Expediente ",
-        "fecha_it": "Fecha de I.T ",
-        "numero_it": "Número de I.T",
-        "fecha_oficio": "Fecha Oficio",
-        "numero_oficio": "Número Oficio",
-        "id_sector": "Id_Sector",
-        "nombre_sector": "nombre_sector",
-        "id_pliego": "Id_Pliego",
-        "nombre_pliego": "nombre_pliego",
-        "codigo": "Id_UE",
-    }
+APP_TO_EXCEL_HEADERS = {
+    "año": "Año",
+    "ng1": "N G 1",
+    "ng2": "N G 2",
+    "fecha_recepcion": "Fecha de recepción",
+    "periodo": "Periodo PEI ",
+    "vigencia": "Vigencia",
+    "tipo_pei": "Tipo de PEI",
+    "estado": "Estado ",
+    "responsable_institucional": "Responsable Institucional ",
+    "cantidad_revisiones": "Cantidad de revisiones",
+    "fecha_derivacion": "Fecha de derivación ",
+    "etapa_revision": "Etapas de revisión",
+    "comentario": "Comentario adicional/ Emisor de I.T",
+    "articulacion": "Articulación ",
+    "expediente": "Expediente ",
+    "fecha_it": "Fecha de I.T ",
+    "numero_it": "Número de I.T",
+    "fecha_oficio": "Fecha Oficio",
+    "numero_oficio": "Número Oficio",
+    "id_sector": "Id_Sector",
+    "nombre_sector": "nombre_sector",
+    "id_pliego": "Id_Pliego",
+    "nombre_pliego": "nombre_pliego",
+    "codigo": "Id_UE",
+}
 
 
 def map_row_to_excel_headers(app_row: dict) -> dict:
