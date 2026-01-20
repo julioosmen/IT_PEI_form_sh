9cf1f2cad76ad0dad614bd864f80361a60e78902
Julio Osores <julio.osores61@gmail.com>
2026-01-20 15:44:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -475,7 +475,7 @@ if "modo" in st.session_state and seleccion:
 
                 fecha_derivacion = st.date_input(
                     "Fecha de derivación",
-                    value=form["fecha_derivacion"] if form["fecha_derivacion"] else datetime.now().date()
+                    value=form.get["fecha_derivacion"],
                 )
 
             with col3:
